b115edb8545518c91132ac3e77067a01312ba614
Alex Woo <alex23woo@gmail.com>
2023-12-28 22:33:51 +0800

    Update 001-monitoring.tf

--- tf/applications/infra-app/001-monitoring.tf
+++ tf/applications/infra-app/001-monitoring.tf
@@ -1,7 +1,14 @@
 # This is a tf mainly for monitoring tools
+# TODO: add variables
+# ============= docker_network ==========
+resource "docker_network" "monitoring_network" {
+  name = "monitoring_network"
+  driver = "bridge"
+}
+# =============== Resources for Prometheus ========================
 # podman run -d -p 9090:9090 -v C:\Users\Alex\Documents\Container-services\Prometheus\prometheus.yml:/etc/prometheus/prometheus.yml -v prometheus-data:/prometheus  prom/prometheus
 resource "docker_image" "prometheus" {
-  name         = "prom/prometheus:latest"
+  name         = "prom/prometheus:v2.48.1"
   keep_locally = false
 }
 
@@ -14,6 +21,11 @@ resource "docker_volume" "prometheus-data" {
 resource "docker_container" "prometheus" {
   image = docker_image.prometheus.image_id
   name  = "lab-prometheus"
+
+  networks_advanced {
+    name    = docker_network.monitoring_network.name
+    aliases = ["prometheus"]
+  }
 
   ports {
     internal = 9090
@@ -32,3 +44,43 @@ resource "docker_container" "prometheus" {
     volume_name = "${docker_volume.prometheus-data.name}"
   }
 }
+
+# =============== Resources for Grafana =========
+
+resource "docker_image" "grafana" {
+  name         = "grafana/grafana-oss:9.5.15"
+  keep_locally = false
+}
+
+
+resource "docker_volume" "grafana-data" {
+  name = "grafana-data"
+}
+
+
+resource "docker_container" "grafana" {
+  image = docker_image.grafana.image_id
+  name  = "lab-grafana"
+  privileged = false
+  publish_all_ports = false
+  user = "472" 
+  working_dir = "/usr/share/grafana"
+  cpu_shares = 0
+
+  networks_advanced {
+    name    = docker_network.monitoring_network.name
+    aliases = ["grafana"]
+  }
+
+  ports {
+    internal = 3000
+    external = 3000
+  }
+  env = null
+  volumes {
+    container_path  = "/var/lib/grafana"
+    read_only = false
+    volume_name = "${docker_volume.grafana-data.name}"
+  }
+}
+
